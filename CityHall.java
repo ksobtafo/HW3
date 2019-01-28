cee33a66fc4db708f2fb62a1e99c73f57050e70c
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-27 19:48:24 -0800

    Create CityHall.java

---
+++ CityHall.java
@@ -0,0 +1,15 @@
+package project1;
+
+public class CityHall {
+	private String policemanName;
+	
+	public CityHall ( String newPolicemanName)
+	{
+		policemanName= newPolicemanName;
+	}
+public String getPoliceName() 
+{
+	return policemanName;
+}
+
+}
