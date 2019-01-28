6736a348353e90f7b039a78566d74792277ce268
Karen Sobtafo <43358183+ksobtafo@users.noreply.github.com>
2019-01-27 19:46:46 -0800

    Create city1.java

---
+++ city1.java
@@ -0,0 +1,83 @@
+package project1;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class city1 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					city1 window = new city1();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public city1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblNewLabel = new JLabel("Welcome to MyCity");
+		lblNewLabel.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 30));
+		lblNewLabel.setForeground(Color.BLACK);
+		lblNewLabel.setBackground(new Color(0, 0, 255));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(lblNewLabel, BorderLayout.CENTER);
+		
+		JButton btnNewButton = new JButton("Let's Go");
+		frame.getContentPane().add(btnNewButton, BorderLayout.SOUTH);
+		btnNewButton.setIcon(new ImageIcon("city.png"));
+		
+		JLabel label = new JLabel("");
+		frame.getContentPane().add(label, BorderLayout.NORTH);
+		label.setIcon(new ImageIcon("city.png"));
+		
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//here
+				City2 cityobj1 = new City2();
+				cityobj1.setVisible(true);
+			}
+		});
+
+		
+		
+		
+		
+	}
+
+}
